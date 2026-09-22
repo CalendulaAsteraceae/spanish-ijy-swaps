24615493b84d4c6c420fb7cd06234639635a48f3
CalendulaAsteraceae <calendulaasteraceae@gmail.com>
2026-09-22 17:28:50 -0500

    Update lua.lua

--- evidence_table_data/lua.lua
+++ evidence_table_data/lua.lua
@@ -1,6 +1,25 @@
 local p = {}
 
-local evidence_table_data = require("evidence_table_data.lua")
 local word_filters = require("word_filters.lua")
+local ijy_corde_forms = require("ijy_corde_forms.lua")
+
+local data_of_interest = {}
+for i, form in ipairs(ijy_corde_forms) do
+	for header, data in pairs(word_filters.word_patterns) do
+		if string.match(form["Transcripción"], data["Pattern"]) then
+			table.insert(
+				data_of_interest,
+				{
+					["Transcripción"] = form["Transcripción"],
+					["Forma"] = form["Forma"],
+					["Datos"] = form["Datos"],
+					["Header"] = header,
+					["Class"] = data["Class"],
+					["Pattern"] = data["Pattern"]
+				}
+			)
+		end
+	end
+end
 
 return p
