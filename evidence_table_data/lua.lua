3a42cdc4e29ccf9c9f6cdf422744ab6dc41c6ffb
CalendulaAsteraceae <calendulaasteraceae@gmail.com>
2026-09-22 22:40:32 -0500

    Update lua.lua

--- evidence_table_data/lua.lua
+++ evidence_table_data/lua.lua
@@ -3,25 +3,7 @@ local p = {}
 local word_filters = require("word_filters.lua")
 local ijy_corde_forms = require("ijy_corde_forms.lua")
 
-local data_to_process = {}
-for i, form in ipairs(ijy_corde_forms) do
-	for correspondencia, data in pairs(word_filters["word_patterns"]) do
-		if string.match(form["Transcripción"], data["Pattern"]) then
-			table.insert(
-				data_to_process,
-				{
-					["Transcripción"] = form["Transcripción"],
-					["Forma"] = form["Forma"],
-					["Datos"] = form["Datos"],
-					["Correspondencia"] = correspondencia,
-					["Clase"] = data["Class"]
-				}
-			)
-		end
-	end
-end
-
-local function letter_for_class(class, data)
+local function word_data(class, data)
 	local letra
 	if class == "g-" or class == "i-" or class == "y.-" then
 		letra = string.match(data["Transcripción"], "^h?(" .. word_filters["patterns"]["swapletters"] .. ")")
@@ -34,42 +16,32 @@ local function letter_for_class(class, data)
 			letra = string.match(data["Transcripción"], word_filters["word_medial_patterns"][class])
 		end
 	end
-	return letra
+	if letra then
+		return {
+			["Transcripción"] = data["Transcripción"],
+			["Forma"] = data["Forma"],
+			["Datos"] = data["Datos"],
+			["Correspondencia"] = data["Correspondencia"],
+			["Clase"] = class,
+			["Letra"] = letra
+		}
+	end
+	return nil
 end
 
 local representative_words = {}
-for i, data in ipairs(data_to_process) do
-	if #data["Clase"] == 1 then
-		local class = data["Clase"][1]
-		local letra = letter_for_class(class, data)
-		if letra then
-			table.insert(
-				representative_words,
-				{
-					["Transcripción"] = data["Transcripción"],
-					["Forma"] = data["Forma"],
-					["Datos"] = data["Datos"],
-					["Correspondencia"] = data["Correspondencia"],
-					["Clase"] = class,
-					["Letra"] = letra
-				}
-			)
-		end
-	elseif #data["Clase"] > 1 then
-		for j, class in ipairs(data["Clase"]) do
-			local letra = letter_for_class(class, data)
-			if letra then
-				table.insert(
-					representative_words,
-					{
-						["Transcripción"] = data["Transcripción"],
-						["Forma"] = data["Forma"],
-						["Datos"] = data["Datos"],
-						["Correspondencia"] = data["Correspondencia"],
-						["Clase"] = class,
-						["Letra"] = letra
-					}
-				)
+for i, form in ipairs(ijy_corde_forms) do
+	for correspondencia, data in pairs(word_filters["word_patterns"]) do
+		if string.match(form["Transcripción"], data["Pattern"]) then
+			local classes = data["Class"]
+			local merged_data = {
+				["Transcripción"] = form["Transcripción"],
+				["Forma"] = form["Forma"],
+				["Datos"] = form["Datos"],
+				["Correspondencia"] = correspondencia
+			}
+			for j, class in ipairs(classes) do
+				table.insert(data_to_process, word_data(class, merged_data))
 			end
 		end
 	end
