25ced66844df83a5090301abbd0d700b189dac19
CalendulaAsteraceae <calendulaasteraceae@gmail.com>
2026-09-22 19:22:10 -0500

    work on data filtering

--- evidence_table_data/lua.lua
+++ evidence_table_data/lua.lua
@@ -3,19 +3,66 @@ local p = {}
 local word_filters = require("word_filters.lua")
 local ijy_corde_forms = require("ijy_corde_forms.lua")
 
-local data_of_interest = {}
+local data_to_process = {}
 for i, form in ipairs(ijy_corde_forms) do
-	for header, data in pairs(word_filters.word_patterns) do
+	for correspondencia, data in pairs(word_filters.word_patterns) do
 		if string.match(form["Transcripción"], data["Pattern"]) then
 			table.insert(
-				data_of_interest,
+				data_to_process,
 				{
 					["Transcripción"] = form["Transcripción"],
 					["Forma"] = form["Forma"],
 					["Datos"] = form["Datos"],
-					["Header"] = header,
-					["Class"] = data["Class"],
-					["Pattern"] = data["Pattern"]
+					["Correspondencia"] = correspondencia,
+					["Clase"] = data["Class"]
+				}
+			)
+		end
+	end
+end
+
+local representative_words = {}
+
+for i, data in ipairs(data_to_process) do
+	if #data["Clase"] == 1 then
+		table.insert(
+			representative_words,
+			{
+				["Transcripción"] = data["Transcripción"],
+				["Forma"] = data["Forma"],
+				["Datos"] = data["Datos"],
+				["Correspondencia"] = data["Correspondencia"],
+				["Clase"] = data["Clase"],
+				["Letra"] = string.match(data["Transcripción"], "(" .. word_filters.patterns.swapletters .. ")")
+			}
+		)
+	else
+		for j, class in ipairs(data["Clase"]) do
+			local letra
+			if class == "g-" or class == "i-" or class == "y.-" then
+				if string.sub(data["Transcripción"], 1, 1) == "h" then
+					letra = string.sub(data["Transcripción"], 2, 2)
+				else
+					letra = string.sub(data["Transcripción"], 1, 1)
+				end
+			elseif class == "-i" or class == "-.y" then
+				letra = string.sub(data["Transcripción"], -1, -1)
+			elseif class == "-g-" or class == "-i-" or class == "-.y-" or class == "-y.-" then
+				if word_filters.manual_word_medial_patterns[class][data["Correspondencia"]] then
+					letra = word_filters.manual_word_medial_patterns[class][data["Correspondencia"]](data["Transcripción"])
+				else
+					letra = string.match(data["Transcripción"], word_filters.word_medial_patterns[class])
+				end
+			end
+			table.insert(
+				representative_words,
+				{
+					["Transcripción"] = data["Transcripción"],
+					["Forma"] = data["Forma"],
+					["Datos"] = data["Datos"],
+					["Correspondencia"] = data["Correspondencia"],
+					["Clase"] = data["Clase"],
+					["Letra"] = letra
 				}
 			)
 		end
--- word_filters/lua.lua
+++ word_filters/lua.lua
@@ -442,4 +442,40 @@ word_filters.word_patterns = {
 	}
 }
 
+word_filters.word_medial_patterns = {
+	["-g-"] = "%w(" .. word_filters.patterns["g"] .. ")%w",
+	["-i-"] = "%w(" .. word_filters.patterns["i"] .. ")%w",
+	["-.y-"] = "[aeou](" .. word_filters.patterns["i"] .. ")%w",
+	["-y.-"] = "%w(" .. word_filters.patterns["i"] .. ")[aeo]"
+}
+
+word_filters.manual_word_medial_patterns = {
+	["-g-"] = {
+	},
+	["-i-"] = {
+		["Fuisteis"] = function(word)
+			return string.match(word, "^f(" .. word_filters.patterns["u"] .. word_filters.patterns["i"] .. ")")
+		end,
+		["Juicio(s)?"] = function(word)
+			return string.match(word, "^(" .. word_filters.patterns["g"] .. word_filters.patterns["u"] .. word_filters.patterns["i"] .. ")")
+		end,
+		["Hubisteis"] = function(word)
+			return string.match(word, "^(h" .. word_filters.patterns["u"] .. word_filters.patterns["b"] .. word_filters.patterns["i"] .. ")")
+		end,
+		["Herir(a|e)is"] = function(word)
+			return string.match(word, "^(her" .. word_filters.patterns["i"] .. ")r")
+		end,
+		["Hicisteis"] = function(word)
+			return string.match(word, "^(h" .. word_filters.patterns["i"] .. word_filters.patterns["s"] .. word_filters.patterns["i"] .. word_filters.patterns["s"] .. ")")
+		end
+	},
+	["-.y-"] = {
+		["Fuisteis"] = function(word)
+			return string.match(word, "(e" .. word_filters.patterns["i"] .. word_filters.patterns["s"] .. ")$")
+		end
+	},
+	["-y.-"] = {
+	}
+}
+
 return word_filters
